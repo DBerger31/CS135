46cd75e0953a5d2ce08a85f3a17e7d526a3dae8a
DBerger31 <37965938+DBerger31@users.noreply.github.com>
2018-12-05 21:42:42 -0500

    Add files via upload

--- lab12/social3.cpp
+++ lab12/social3.cpp
@@ -59,6 +59,8 @@ private:
    int numUsers; // number of registered users
    //returns user ID (index in the profiles array) by their username or -1 if username not found
    int findID (string usrn);
+   bool following[MAX_USERS][MAX_USERS]; // Friendship matrix
+   //follwing[id1][id2] == true when id1 is following id2
 
 public:
 
@@ -67,11 +69,22 @@ public:
    // Attempts to sign up a new user with specified username and displayname
    // return true if the operation was successful, otherwise return false
    bool addUser(string usrn, string dspn);
+   // Make 'usrn1' follow 'usrn2' (if both usernames are in the network).
+   // return true if success (if both usernames exist), otherwise return false
+   bool follow (string usrn1, string usrn2);
+   // Print Dot file (graphical representation of the network)
+   void printDot();
 };
 
-Network::Network() : numUsers (0) { }
+Network::Network() : numUsers (0) {
+   for (int i = 0; i < MAX_USERS; i++) {
+      for (int j = 0; j < MAX_USERS; j++) {
+         following[i][j] = false;
+      }
+   }
+}
 
-bool Network:: addUser(string usrn, string dspn) {
+bool Network:: addUser (string usrn, string dspn) {
    static int index = 0;
    if (index < MAX_USERS) {
       if ((usrn != "")) {
@@ -95,7 +108,6 @@ bool Network:: addUser(string usrn, string dspn) {
    else { return false;}
 }
 
-
 int Network:: findID (string usrn) {
    for (int i = 0; i < MAX_USERS; i++) {
       if (profiles[i].getUsername() == usrn) {
@@ -105,23 +117,60 @@ int Network:: findID (string usrn) {
    return -1;
 }
 
+bool Network:: follow (string usrn1, string usrn2) {
+   if ((findID(usrn1) != -1) && (findID(usrn2) != -1)) { // if both usernames exist
+      following[findID(usrn1)][findID(usrn2)] = true;
+      return true;
+   }
+   else {
+      return false;}
+}
+
+void Network:: printDot() {
+   cout << "digraph {" << endl;
+   for (int i = 0; i < MAX_USERS; i++) {
+      if (profiles[i].getUsername() != "") {
+         cout << "   \"@" + profiles[i].getUsername() + "\""<< endl;
+      }
+   }
+
+   for (int i = 0; i < MAX_USERS; i++) {
+      for (int j = 0; j < MAX_USERS; j++) {
+         if (following[i][j]) { // if true
+            cout << "   \"@" + profiles[i].getUsername() + "\" -> \"@" + profiles[j].getUsername() + "\"" << endl;
+         }
+      }
+   }
+   cout << "}" << endl;
+}
 
 int main() {
-  Network nw;
-  cout << nw.addUser("mario", "Mario") << endl;     // true (1)
-  cout << nw.addUser("luigi", "Luigi") << endl;     // true (1)
+    Network nw;
+    // add three users
+    nw.addUser("mario", "Mario");
+    nw.addUser("luigi", "Luigi");
+    nw.addUser("yoshi", "Yoshi");
 
-  cout << nw.addUser("mario", "Mario2") << endl;    // false (0)
-  cout << nw.addUser("mario 2", "Mario2") << endl;  // false (0)
-  cout << nw.addUser("mario-2", "Mario2") << endl;  // false (0)
+    // make them follow each other
+    nw.follow("mario", "luigi");
+    nw.follow("mario", "yoshi");
+    nw.follow("luigi", "mario");
+    nw.follow("luigi", "yoshi");
+    nw.follow("yoshi", "mario");
+    nw.follow("yoshi", "luigi");
 
-  for(int i = 2; i < 20; i++)
-      cout << nw.addUser("mario" + to_string(i),
-                 "Mario" + to_string(i)) << endl;   // true (1)
+    // add a user who does not follow others
+    nw.addUser("wario", "Wario");
 
-  cout << nw.addUser("yoshi", "Yoshi") << endl;     // false (0)
+    // add clone users who follow @mario
+    for(int i = 2; i < 6; i++) {
+        string usrn = "mario" + to_string(i);
+        string dspn = "Mario " + to_string(i);
+        nw.addUser(usrn, dspn);
+        nw.follow(usrn, "mario");
+    }
+    // additionally, make @mario2 follow @luigi
+    nw.follow("mario2", "luigi");
 
-  cout << "the user id is " << nw.findID("mario") << endl;
-  cout << "the user id is " << nw.findID("luigi") << endl;
-
+    nw.printDot();
 }
